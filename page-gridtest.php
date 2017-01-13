5f0ad7ea9847b3e0e4b12b5e0279522534302966
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2017-01-13 02:34:39 -0600

    head, page, footer complete

---
+++ page-gridtest.php
@@ -0,0 +1,56 @@
+<div class="page-wrap">
+    <div class="grid">
+        <div class="row white">
+            <div class="twelve-twelve columns">
+                <h1>sample</h1></div>
+        </div>
+        <div class="row">
+            <div class="three-twelve columns white">
+                <h2>1/4</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin feugiat ultricies. Praesent a rhoncus metus. Nullam eleifend elementum viverra. Proin venenatis odio in odio scelerisque, vitae accumsan sem maximus.</p>
+            </div>
+            <div class="three-twelve columns white">
+                <h2>2/4</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin feugiat ultricies. Praesent a rhoncus metus. Nullam eleifend elementum viverra. Proin venenatis odio in odio scelerisque, vitae accumsan sem maximus. Duis in pretium erat. Donec rhoncus ex laoreet urna luctus luctus. Aliquam tempor tortor metus, ut tristique leo tempor ut. Duis ullamcorper, nulla vel maximus pellentesque, nisl odio maximus metus, ac ultrices sem tortor egestas ipsum. Nam at imperdiet lectus. Suspendisse at diam mi. Nullam ut ex maximus, luctus ante sit amet, condimentum nisi. Duis nec maximus nulla.</p>
+            </div>
+            <div class="three-twelve columns white">
+                <h2>3/4</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin feugiat ultricies. Praesent a rhoncus metus. Nullam eleifend elementum viverra. Proin venenatis odio in odio scelerisque, vitae accumsan sem maximus. Duis in pretium erat. Donec rhoncus ex laoreet urna luctus luctus. Aliquam tempor tortor metus, ut tristique leo tempor ut. </p>
+            </div>
+            <div class="three-twelve columns white">
+                <h2>4/4</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="twelve-twelve columns white">
+           w     <h2>1/1</h2>
+                <p>text</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="four-twelve columns white">
+                <h2>1/3</h2>
+                <p>text</p>
+            </div>
+            <div class="four-twelve columns white">
+                <h2>2/3</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin feugiat ultricies. Praesent a rhoncus metus. Nullam eleifend elementum viverra. Proin venenatis odio in odio scelerisque, vitae accumsan sem maximus.</p>
+            </div>
+            <div class="four-twelve columns white">
+                <h2>3/3</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin feugiat ultricies. Praesent a rhoncus metus. Nullam eleifend elementum viverra. Proin venenatis odio in odio scelerisque, vitae accumsan sem maximus. Duis in pretium erat. Donec rhoncus ex laoreet urna luctus luctus. Aliquam tempor tortor metus, ut tristique leo tempor ut. Duis ullamcorper, nulla vel maximus pellentesque, nisl odio maximus metus, ac ultrices sem tortor egestas ipsum. Nam at imperdiet lectus. Suspendisse at diam mi. Nullam ut ex maximus, luctus ante sit amet, condimentum nisi. Duis nec maximus nulla.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="six-twelve columns white">
+                <h2>1/2</h2>
+                <p>text</p>
+            </div>
+            <div class="six-twelve columns white">
+                <h2>2/2</h2>
+                <p>text</p>
+            </div>
+        </div>
+    </div>
+</div>
