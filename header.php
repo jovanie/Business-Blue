5dcef71fb616706a6691e565d32797567b620833
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2017-01-13 00:41:16 -0600

    responsive grid js and css are complete

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="<?php bloginfo( 'template_directory' );?>/js/parallax.min.js"></script>
+    <script src="<?php bloginfo( 'template_directory' );?>/js/responsive-grid-height.js"></script>
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
